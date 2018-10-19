7b1e7fdd7ab86e4094490b08eda37245126a8af0
Rene Petersen <r.petersen@tfe.nl>
2018-10-19 22:38:12 +0200

    Init commit

---
+++ App.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { StyleSheet, View, StatusBar } from 'react-native';
+import { createBottomTabNavigator, createStackNavigator } from 'react-navigation' 
+import { Constants } from 'expo'
+
+import Decks from './components/Decks'
+import NewDeck from './components/NewDeck'
+
+import { purple, white } from './constants/colors'
+import { Foundation, MaterialCommunityIcons } from '@expo/vector-icons'
+
+
+function FlashcardsStatusBar ({backgroundColor, ...props}) {
+	return (
+		<View style={{backgroundColor, height: Constants.StatusBarHeight}}>
+			<StatusBar translucent backgroundColor={backgroundColor} {...props} />
+		</View>
+	)
+}
+
+const Tabs = createBottomTabNavigator({
+	Decks: {
+		screen: Decks,
+		navigationOptions: {
+			tabBarLabel: 'Decks',
+			tabBarIcon: ({ tintColor }) => <Foundation name='page-multiple' size={30} color={tintColor} />
+		}
+	},
+	NewDeck: {
+		screen: NewDeck,
+		navigationOptions: {
+			tabBarLabel: 'New Deck',
+			tabBarIcon: ({ tintColor }) => <MaterialCommunityIcons name='new-box' size={30} color={tintColor} />
+		}
+	},
+},
+{
+	navigationOptions: {
+		header: null
+	},
+	tabBarOptions: {
+		activeTintColor: purple,
+		style: {
+			height: 56,
+			backgroundColor:  white,
+			shadowColor: 'rgba(0, 0, 0, 0.24)',
+			shadowOffset: {
+				width: 0,
+				height: 3
+			},
+			shadowRadius: 6,
+			shadowOpacity: 1
+		}
+	}
+})
+
+
+export default class App extends React.Component {
+	render() {
+		return (
+			<View style={{flex:1}}>
+				<FlashcardsStatusBar backgroundColor={purple} barStyle='light-content' />
+				<Tabs />
+			</View>
+		);
+	}
+}
---
+++ components/NewDeck.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import { View, Text, StyleSheet} from 'react-native'
+import { purple, white } from '../constants/colors'
+
+class NewDeck extends Component {
+	render() {
+		return (
+			<View style={styles.center}>
+				<Text>NewDeck</Text>
+			</View>
+		)
+	}
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: white
+  },
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 30,
+    marginRight: 30,
+  },
+})
+
+export default NewDeck
